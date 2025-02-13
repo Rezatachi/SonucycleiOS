ba05e18daf1957235b4f9db2f8708aea91718d3c
Abraham Belayneh <abraham@caltech.edu>
2025-02-12 22:50:32 -0800

    Initial Commit

---
+++ SonucycleIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SonucycleIOS
+//
+//  Created by Abraham Belayneh on 2/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SonucycleIOS/SonucycleIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonucycleIOSApp.swift
+//  SonucycleIOS
+//
+//  Created by Abraham Belayneh on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SonucycleIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
